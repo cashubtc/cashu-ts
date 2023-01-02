13128d314b00c09bfdc88602abf59303c5f1cd7a
gandlaf21 <gandlaf@proton.me>
2023-01-02 16:38:20 +0100

    add linting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-      transform: {'^.+\\.ts?$': 'ts-jest'},
-      testEnvironment: 'node',
-      testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-    };
+
+export default {
+    transform: { '^.+\\.ts?$': 'ts-jest' },
+    testEnvironment: 'node',
+    testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
