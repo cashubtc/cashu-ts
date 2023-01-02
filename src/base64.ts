30e5ab8c6c971f6e161aa2621aafe48e00cbbe0f
gandlaf21 <gandlaf@proton.me>
2023-01-02 16:39:21 +0100

    add base64toJson encoding

--- src/base64.ts
+++ src/base64.ts
@@ -9,12 +9,20 @@ function encodeBase64toUint8(base64String: string): Uint8Array {
 }
 
 function encodeJsonToBase64(jsonObj: any): string {
-    const jsonString = JSON.stringify(jsonObj)
+    const jsonString: string = JSON.stringify(jsonObj)
     return  Buffer.from(jsonString).toString('base64')
 }
+
+function encodeBase64ToJson(base64String: string): any {
+    const jsonString = Buffer.from(base64String,'base64').toString()
+    const jsonObj: any = JSON.parse(jsonString)
+    return  jsonObj
+}
+
 
 export {
     encodeUint8toBase64,
     encodeBase64toUint8,
-    encodeJsonToBase64
+    encodeJsonToBase64,
+    encodeBase64ToJson
 }
