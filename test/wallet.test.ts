f973b0ac78f88934625797a17ffbb9259b942eb9
BilligsterUser <billigsteruser@protonmail.com>
2023-03-19 23:43:54 +0100

    Update wallet.test.ts

--- test/wallet.test.ts
+++ test/wallet.test.ts
@@ -7,6 +7,12 @@ import { Proof } from '../src/model/Proof.js';
 // Mock jest and set the type
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+afterEach(() => {
+	mockedAxios.post.mockReset();
+	mockedAxios.get.mockReset();
+});
+
 const mint = new CashuMint('https://legend.lnbits.com', 'cashu/api/v1/4gr9Xcmz3XEkUNwiBiQGoC');
 const invoice =
 	'lnbc20u1p3u27nppp5pm074ffk6m42lvae8c6847z7xuvhyknwgkk7pzdce47grf2ksqwsdpv2phhwetjv4jzqcneypqyc6t8dp6xu6twva2xjuzzda6qcqzpgxqyz5vqsp5sw6n7cztudpl5m5jv3z6dtqpt2zhd3q6dwgftey9qxv09w82rgjq9qyyssqhtfl8wv7scwp5flqvmgjjh20nf6utvv5daw5h43h69yqfwjch7wnra3cn94qkscgewa33wvfh7guz76rzsfg9pwlk8mqd27wavf2udsq3yeuju';
@@ -209,6 +215,7 @@ describe('requestTokens', () => {
 		}
 	});
 });
+
 describe('send', () => {
 	const proofs = [
 		new Proof(
@@ -225,18 +232,16 @@ describe('send', () => {
 		const keys = await mint.getKeys();
 		const wallet = new CashuWallet(keys, mint);
 		mockedAxios.post.mockResolvedValueOnce({
-			data: mockedAxios.post.mockResolvedValueOnce({
-				data: {
-					fst: [],
-					snd: [
-						{
-							id: '/uYB/6wWnYkU',
-							amount: 1,
-							C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
-						}
-					]
-				}
-			})
+			data: {
+				fst: [],
+				snd: [
+					{
+						id: '/uYB/6wWnYkU',
+						amount: 1,
+						C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
+					}
+				]
+			}
 		});
 		const result = await wallet.send(1, proofs);
 		expect(result.returnChange).toHaveLength(0);
@@ -252,23 +257,38 @@ describe('send', () => {
 		const keys = await mint.getKeys();
 		const wallet = new CashuWallet(keys, mint);
 		mockedAxios.post.mockResolvedValueOnce({
-			data: mockedAxios.post.mockResolvedValueOnce({
-				data: {
-					fst: [],
-					snd: [
-						{
-							id: '/uYB/6wWnYkU',
-							amount: 1,
-							C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
-						}
-					]
-				}
-			})
+			data: {
+				fst: [
+					{
+						id: '/uYB/6wWnYkU',
+						amount: 1,
+						C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
+					}
+				],
+				snd: [
+					{
+						id: '/uYB/6wWnYkU',
+						amount: 1,
+						C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
+					}
+				]
+			}
 		});
-		const result = await wallet.send(0, proofs);
-		expect(result.send).toHaveLength(0);
+		const result = await wallet.send(1, [
+			new Proof(
+				'/uYB/6wWnYkU',
+				2,
+				'H5jmg3pDRkTJQRgl18bW4Tl0uTH48GUiF86ikBBnShM=',
+				'034268c0bd30b945adf578aca2dc0d1e26ef089869aaf9a08ba3a6da40fda1d8be'
+			)
+		]);
+
+		expect(result.send).toHaveLength(1);
+		expect(result.send[0]).toMatchObject({ amount: 1, id: '/uYB/6wWnYkU' });
+		expect(/[0-9a-f]{64}/.test(result.send[0].C)).toBe(true);
+		expect(/[A-Za-z0-9+/]{43}=/.test(result.send[0].secret)).toBe(true);
 		expect(result.returnChange).toHaveLength(1);
-		expect(result.returnChange[0]).toMatchObject({ amount: 1, id: '0NI3TUAs1Sfy' });
+		expect(result.returnChange[0]).toMatchObject({ amount: 1, id: '/uYB/6wWnYkU' });
 		expect(/[0-9a-f]{64}/.test(result.returnChange[0].C)).toBe(true);
 		expect(/[A-Za-z0-9+/]{43}=/.test(result.returnChange[0].secret)).toBe(true);
 	});
@@ -279,18 +299,16 @@ describe('send', () => {
 		const keys = await mint.getKeys();
 		const wallet = new CashuWallet(keys, mint);
 		mockedAxios.post.mockResolvedValueOnce({
-			data: mockedAxios.post.mockResolvedValueOnce({
-				data: {
-					fst: [],
-					snd: [
-						{
-							id: '/uYB/6wWnYkU',
-							amount: 1,
-							C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
-						}
-					]
-				}
-			})
+			data: {
+				fst: [],
+				snd: [
+					{
+						id: '/uYB/6wWnYkU',
+						amount: 1,
+						C_: '021179b095a67380ab3285424b563b7aab9818bd38068e1930641b3dceb364d422'
+					}
+				]
+			}
 		});
 		try {
 			await wallet.send(2, proofs);
