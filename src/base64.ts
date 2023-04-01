393716e84d5b88ea1fe9ec3c46058c47e488353f
BilligsterUser <billigsteruser@protonmail.com>
2023-04-01 17:53:30 +0200

    add base64 helper and test

--- src/base64.ts
+++ src/base64.ts
@@ -10,13 +10,21 @@ function encodeBase64toUint8(base64String: string): Uint8Array {
 
 function encodeJsonToBase64(jsonObj: unknown): string {
 	const jsonString = JSON.stringify(jsonObj);
-	return Buffer.from(jsonString).toString('base64url');
+	return base64urlFromBase64(Buffer.from(jsonString).toString('base64'));
 }
 
 function encodeBase64ToJson<T extends object>(base64String: string): T {
-	const jsonString = Buffer.from(base64String, 'base64url').toString();
+	const jsonString = Buffer.from(base64urlToBase64(base64String), 'base64').toString();
 	const jsonObj = JSON.parse(jsonString) as T;
 	return jsonObj;
+}
+
+function base64urlToBase64(str: string) {
+	return str.replace(/-/g, '+').replace(/_/g, '/');
+}
+
+function base64urlFromBase64(str: string) {
+	return str.replace(/\+/g, '-').replace(/\//g, '_').split('=')[0];
 }
 
 export { encodeUint8toBase64, encodeBase64toUint8, encodeJsonToBase64, encodeBase64ToJson };
