b8fe4ececa60b698c7e34b47b1e1637ca35152ce
BilligsterUser <billigsteruser@protonmail.com>
2023-05-01 12:47:46 +0200

    add TokenEntry type

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Proof } from './model/types/index.js';
 import { getEncodedToken, getDecodedToken, deriveKeysetId } from './utils.js';
 import { decode as getDecodedLnInvoice } from '@gandlaf21/bolt11-decode';
 
+export * from './model/types/index.js';
+
 export {
 	CashuMint,
 	CashuWallet,
--- src/model/types/index.ts
+++ src/model/types/index.ts
@@ -71,9 +71,14 @@ export type SerializedBlindedSignature = {
 };
 
 export type Token = {
-	token: Array<{ mint: string; proofs: Array<Proof> }>;
+	token: Array<TokenEntry>;
 	memo?: string;
 };
+
+export type TokenEntry = {
+	proofs: Array<Proof>
+	mint: string
+}
 
 export type TokenV2 = {
 	proofs: Array<Proof>;
