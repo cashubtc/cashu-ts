aa0b375037cf671aab6b398e4b7d5dfcf9cbee93
sdbowen <sdbowen.git@gmail.com>
2023-09-02 10:33:57 -0400

    Add fetch wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { CashuMint } from './CashuMint.js';
 import { CashuWallet } from './CashuWallet.js';
+import { setGlobalRequestOptions } from './request.js';
 import { getEncodedToken, getDecodedToken, deriveKeysetId } from './utils.js';
 import { decode as getDecodedLnInvoice } from '@gandlaf21/bolt11-decode';
 
@@ -12,4 +13,5 @@ export {
 	getEncodedToken,
 	deriveKeysetId,
 	getDecodedLnInvoice,
+	setGlobalRequestOptions
 };
