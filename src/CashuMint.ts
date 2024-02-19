348d6943022953d9a5744f30fc01952300b2b96b
gandlaf21 <gandlaf@proton.me>
2024-02-19 16:34:08 +0900

    format

--- src/CashuMint.ts
+++ src/CashuMint.ts
@@ -23,10 +23,7 @@ class CashuMint {
 	 * @param _mintUrl requires mint URL to create this object
 	 * @param _customRequest if passed, use custom request implementation for network communication with the mint
 	 */
-	constructor(
-		private _mintUrl: string,
-		private _customRequest?: typeof request
-	) {}
+	constructor(private _mintUrl: string, private _customRequest?: typeof request) {}
 
 	get mintUrl() {
 		return this._mintUrl;
