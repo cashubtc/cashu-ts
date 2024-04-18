4387513301032e53eb4ae5a8e83760094a76045b
Egge <egge@getcurrent.io>
2024-04-18 15:10:03 +0200

    added jsonrpc types

--- src/model/types/index.ts
+++ src/model/types/index.ts
@@ -17,3 +17,40 @@ export type InvoiceData = {
 	memo?: string;
 	expiry?: number;
 };
+
+export type RpcSubId = string | number | null;
+
+type JsonRpcParams = any;
+
+type JsonRpcSuccess<T = any> = {
+	jsonrpc: '2.0';
+	result: T;
+	id: RpcSubId;
+};
+
+export type JsonRpcErrorObject = {
+	code: number;
+	message: string;
+	data?: any;
+};
+
+type JsonRpcError = {
+	jsonrpc: '2.0';
+	error: JsonRpcErrorObject;
+	id: RpcSubId;
+};
+
+type JsonRpcRequest = {
+	jsonrpc: '2.0';
+	method: string;
+	params?: JsonRpcParams;
+	id: Exclude<RpcSubId, null>;
+};
+
+type JsonRpcNotification = {
+	jsonrpc: '2.0';
+	method: string;
+	params?: JsonRpcParams;
+};
+
+export type JsonRpcMessage = JsonRpcRequest | JsonRpcNotification | JsonRpcSuccess | JsonRpcError;
