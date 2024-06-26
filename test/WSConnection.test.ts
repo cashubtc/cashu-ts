fcef8dab1961e869ba3f4adbcf2c7057e140969b
Egge <egge@getcurrent.io>
2024-06-26 17:37:53 +0200

    added error handling

--- test/WSConnection.test.ts
+++ test/WSConnection.test.ts
@@ -4,8 +4,19 @@ import { CashuMint, CashuWallet } from '../src/index';
 describe('testing WSConnection', () => {
 	test('connecting...', async () => {
 		injectWebSocketImpl(require('ws'));
-		const wallet = new CashuWallet(new CashuMint('ws://localhost:3338'));
-		const quote = await wallet.getMintQuote(21);
-		console.log(quote);
+		const wallet = new CashuWallet(new CashuMint('http://localhost:3338'));
+		await new Promise((res, rej) => {
+			const unsub = wallet.onQuotePaid(
+				'XCV',
+				(pa) => {
+					console.log(pa);
+					res(pa);
+				},
+				(e: Error) => {
+					rej(e);
+				}
+			);
+		});
+		console.log('Ended');
 	});
 });
