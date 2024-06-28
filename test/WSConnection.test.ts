1a23818955872fdb0384ee71e9181163537910a1
Egge <egge@getcurrent.io>
2024-06-28 09:13:00 +0200

    added some tests

--- test/WSConnection.test.ts
+++ test/WSConnection.test.ts
@@ -1,22 +1,71 @@
 import { WSConnection, injectWebSocketImpl } from '../src/WSConnection';
-import { CashuMint, CashuWallet } from '../src/index';
+import { Server, WebSocket } from 'mock-socket';
+
+injectWebSocketImpl(WebSocket);
 
 describe('testing WSConnection', () => {
 	test('connecting...', async () => {
-		injectWebSocketImpl(require('ws'));
-		const wallet = new CashuWallet(new CashuMint('http://localhost:3338'));
-		await new Promise((res, rej) => {
-			const unsub = wallet.onQuotePaid(
-				'XCV',
-				(pa) => {
-					console.log(pa);
-					res(pa);
-				},
-				(e: Error) => {
-					rej(e);
-				}
+		const fakeUrl = 'ws://localhost:3338/v1/ws';
+		const server = new Server(fakeUrl, { mock: false });
+		const connectionSpy = jest.fn();
+		server.on('connection', connectionSpy);
+		const conn = new WSConnection(fakeUrl);
+		await conn.connect();
+		expect(connectionSpy).toHaveBeenCalled();
+		server.stop();
+	});
+	test('requesting subscription', async () => {
+		const fakeUrl = 'ws://localhost:3338/v1/ws';
+		const server = new Server(fakeUrl, { mock: false });
+		const message = (await new Promise(async (res) => {
+			server.on('connection', (socket) => {
+				socket.on('message', (m) => {
+					res(m.toString());
+				});
+			});
+			const conn = new WSConnection(fakeUrl);
+			await conn.connect();
+
+			const callback = jest.fn();
+			const errorCallback = jest.fn();
+			conn.createSubscription(
+				{ kind: 'bolt11_mint_quote', filters: ['12345'] },
+				callback,
+				errorCallback
+			);
+		})) as string;
+		expect(JSON.parse(message)).toMatchObject({
+			jsonrpc: '2.0',
+			method: 'subscribe',
+			params: { kind: 'bolt11_mint_quote', filters: ['12345'] }
+		});
+		server.stop();
+	});
+	test('unsubscribing', async () => {
+		const fakeUrl = 'ws://localhost:3338/v1/ws';
+		const server = new Server(fakeUrl, { mock: false });
+		const message = await new Promise(async (res) => {
+			server.on('connection', (socket) => {
+				socket.on('message', (m) => {
+					const parsed = JSON.parse(m.toString());
+					if (parsed.method === 'unsubscribe') res(parsed);
+				});
+			});
+			const conn = new WSConnection(fakeUrl);
+			await conn.connect();
+
+			conn.sendRequest('subscribe', {
+				subId: '12345',
+				kind: 'bolt11_mint_quote',
+				filters: ['12345']
+			});
+			const callback = jest.fn();
+			const errorCallback = jest.fn();
+			conn.createSubscription(
+				{ kind: 'bolt11_mint_quote', filters: ['123'] },
+				callback,
+				errorCallback
 			);
 		});
-		console.log('Ended');
 	});
 });
