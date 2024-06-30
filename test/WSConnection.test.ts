cf60dedde106d1c3408e24b4935f0889ac528bf5
Egge <egge@getcurrent.io>
2024-06-30 06:02:42 +0200

    more tests

--- test/WSConnection.test.ts
+++ test/WSConnection.test.ts
@@ -54,14 +54,52 @@ describe('testing WSConnection', () => {
 			const conn = new WSConnection(fakeUrl);
 			await conn.connect();
 
-			conn.sendRequest('subscribe', {
-				subId: '12345',
-				kind: 'bolt11_mint_quote',
-				filters: ['12345']
-			});
 			const callback = jest.fn();
 			const errorCallback = jest.fn();
-			conn.createSubscription(
+			const subId = conn.createSubscription(
+				{ kind: 'bolt11_mint_quote', filters: ['123'] },
+				callback,
+				errorCallback
+			);
+			//TODO: Add assertion for subListenerLength once SubscriptionManager is modularised
+			conn.cancelSubscription(subId, callback);
+		});
+		expect(message).toMatchObject({ jsonrpc: '2.0', method: 'unsubscribe' });
+		server.stop();
+	});
+	test('handing a notification', async () => {
+		const fakeUrl = 'ws://localhost:3338/v1/ws';
+		const server = new Server(fakeUrl, { mock: false });
+		server.on('connection', (socket) => {
+			socket.on('message', (m) => {
+				console.log(m);
+				try {
+					const parsed = JSON.parse(m.toString());
+					if (parsed.method === 'subscribe') {
+						const message = `{"jsonrpc": "2.0", "result": {"status": "OK", "subId": "${parsed.params.subId}", "id": ${parsed.id}}}`;
+						console.log(message);
+						socket.send(message);
+						setTimeout(() => {
+							const message = `{"jsonrpc": "2.0", "method": "subscribe", "params": {"subId": "${parsed.params.subId}", "payload": {"quote": "123", "request": "456", "paid": true, "expiry": 123}}}`;
+							console.log(message);
+							socket.send(message);
+						}, 500);
+					}
+				} catch {
+					console.log('Server parsing failed...');
+				}
+			});
+		});
+		const conn = new WSConnection(fakeUrl);
+		await conn.connect();
+
+		await new Promise((res) => {
+			const callback = jest.fn((p) => {
+				console.log('Payload received! ', p);
+				res(p);
+			});
+			const errorCallback = jest.fn();
+			const subId = conn.createSubscription(
 				{ kind: 'bolt11_mint_quote', filters: ['123'] },
 				callback,
 				errorCallback
