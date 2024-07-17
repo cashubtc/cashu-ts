205c33a0f7761da093f434942cb0e36b0005c62c
Egge <egge@getcurrent.io>
2024-07-17 10:53:30 +0200

    added projects

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,22 @@
 export default {
-	transform: { '^.+\\.ts?$': 'ts-jest' },
-	testEnvironment: 'node',
-	resolver: 'ts-jest-resolver',
-	testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	setupFilesAfterEnv: ['./test/_replace-native-fetch.ts']
+	projects: [
+		{
+			displayName: 'node',
+			transform: { '^.+\\.ts?$': 'ts-jest' },
+			testEnvironment: 'node',
+			resolver: 'ts-jest-resolver',
+			testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
+			moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+			setupFilesAfterEnv: ['./test/_replace-native-fetch.ts']
+		},
+		{
+			displayName: 'browser',
+			transform: { '^.+\\.ts?$': 'ts-jest' },
+			testEnvironment: 'jsdom',
+			resolver: 'ts-jest-resolver',
+			testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
+			moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+			setupFilesAfterEnv: ['./test/_replace-native-fetch.ts']
+		}
+	]
 };
