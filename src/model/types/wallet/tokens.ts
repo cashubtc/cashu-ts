8be0932b303dc696afbbc6e659574d7c68beb780
Caleb Beery <cjbeery@gmail.com>
2024-07-17 18:42:59 -0700

    Ran npm run format

--- src/model/types/wallet/tokens.ts
+++ src/model/types/wallet/tokens.ts
@@ -1,32 +1,32 @@
-import {Proof} from "./index";
+import { Proof } from './index';
 
 /**
  * A Cashu token
  */
 export type Token = {
-  /**
-   * token entries
-   */
-  token: Array<TokenEntry>;
-  /**
-   * a message to send along with the token
-   */
-  memo?: string;
-  /**
-   * the unit of the token
-   */
-  unit?: string;
+	/**
+	 * token entries
+	 */
+	token: Array<TokenEntry>;
+	/**
+	 * a message to send along with the token
+	 */
+	memo?: string;
+	/**
+	 * the unit of the token
+	 */
+	unit?: string;
 };
 /**
  * TokenEntry that stores proofs and mints
  */
 export type TokenEntry = {
-  /**
-   * a list of proofs
-   */
-  proofs: Array<Proof>;
-  /**
-   * the mints URL
-   */
-  mint: string;
+	/**
+	 * a list of proofs
+	 */
+	proofs: Array<Proof>;
+	/**
+	 * the mints URL
+	 */
+	mint: string;
 };
