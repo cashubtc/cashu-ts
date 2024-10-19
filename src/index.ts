d9a6b5910a53648eecf29034a09cfcdd45fa8ab5
callebtc <93376500+callebtc@users.noreply.github.com>
2024-10-19 13:35:16 +0200

    format

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import { CashuMint } from './CashuMint.js';
 import { CashuWallet } from './CashuWallet.js';
 import { PaymentRequest } from './model/PaymentRequest.js';
 import { setGlobalRequestOptions } from './request.js';
-import { getEncodedToken, getEncodedTokenV4, getDecodedToken, deriveKeysetId, decodePaymentRequest } from './utils.js';
+import {
+	getEncodedToken,
+	getEncodedTokenV4,
+	getDecodedToken,
+	deriveKeysetId,
+	decodePaymentRequest
+} from './utils.js';
 
 export * from './model/types/index.js';
 
