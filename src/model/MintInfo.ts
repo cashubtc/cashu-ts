911b05cf062f2b66ef667d5a25327540d36f974c
Egge <egge@getcurrent.io>
2024-11-16 23:32:07 +0100

    added optional nuts

--- src/model/MintInfo.ts
+++ src/model/MintInfo.ts
@@ -59,28 +59,50 @@ type GetInfoResponse2 = {
 };
 
 export class MintInfo {
-	private mintInfo: GetInfoResponse;
+	private readonly mintInfo: GetInfoResponse;
 
 	constructor(info: GetInfoResponse) {
 		this.mintInfo = info;
 	}
 
+	isSupported(num: 7 | 8 | 9 | 10 | 11 | 12 | 14): { supported: boolean };
 	isSupported(num: 17): { supported: boolean; params?: Array<WebSocketSupport> };
 	isSupported(num: 15): { supported: boolean; params?: Array<MPPMethod> };
 	isSupported(num: number) {
 		switch (num) {
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+			case 14: {
+				return this.checkGenericNut(num);
+			}
 			case 17: {
 				return this.checkNut17();
 			}
 			case 15: {
+				return this.checkNut15();
 			}
 		}
 	}
-	private checkNut17() {
-		if (this.mintInfo.nuts['17'] && this.mintInfo.nuts[17].supported.length > 0) {
-			return { supported: true, params: this.mintInfo.nuts[17]?.supported };
+	private checkGenericNut(num: 7 | 8 | 9 | 10 | 11 | 12 | 14) {
+		if (this.mintInfo.nuts[num]?.supported) {
+			return { supported: true };
 		}
 		return { supported: false };
 	}
-	private checkNut15() {}
+	private checkNut17() {
+		if (this.mintInfo.nuts['17'] && this.mintInfo.nuts[17].supported.length > 0) {
+			return { supported: true, params: this.mintInfo.nuts[17].supported };
+		}
+		return { supported: false };
+	}
+	private checkNut15() {
+		if (this.mintInfo.nuts['15'] && this.mintInfo.nuts[15].methods.length > 0) {
+			return { supported: true, params: this.mintInfo.nuts[15].methods };
+		}
+		return { supported: false };
+	}
 }
