f5d8b5106d0e4161511660d85242e28a0c4dc38d
Egge <egge@getcurrent.io>
2024-11-18 20:56:44 +0000

    private fields

--- src/model/MintInfo.ts
+++ src/model/MintInfo.ts
@@ -1,10 +1,10 @@
 import { GetInfoResponse, MPPMethod, SwapMethod, WebSocketSupport } from './types';
 
 export class MintInfo {
-	private readonly mintInfo: GetInfoResponse;
+	private readonly _mintInfo: GetInfoResponse;
 
 	constructor(info: GetInfoResponse) {
-		this.mintInfo = info;
+		this._mintInfo = info;
 	}
 
 	isSupported(num: 4 | 5): { disabled: boolean; params: Array<SwapMethod> };
@@ -38,56 +38,56 @@ export class MintInfo {
 		}
 	}
 	private checkGenericNut(num: 7 | 8 | 9 | 10 | 11 | 12 | 14) {
-		if (this.mintInfo.nuts[num]?.supported) {
+		if (this._mintInfo.nuts[num]?.supported) {
 			return { supported: true };
 		}
 		return { supported: false };
 	}
 	private checkMintMelt(num: 4 | 5) {
-		const mintMeltInfo = this.mintInfo.nuts[num];
+		const mintMeltInfo = this._mintInfo.nuts[num];
 		if (mintMeltInfo && mintMeltInfo.methods.length > 0 && !mintMeltInfo.disabled) {
 			return { disabled: false, params: mintMeltInfo.methods };
 		}
 		return { disabled: true, params: mintMeltInfo.methods };
 	}
 	private checkNut17() {
-		if (this.mintInfo.nuts[17] && this.mintInfo.nuts[17].supported.length > 0) {
-			return { supported: true, params: this.mintInfo.nuts[17].supported };
+		if (this._mintInfo.nuts[17] && this._mintInfo.nuts[17].supported.length > 0) {
+			return { supported: true, params: this._mintInfo.nuts[17].supported };
 		}
 		return { supported: false };
 	}
 	private checkNut15() {
-		if (this.mintInfo.nuts[15] && this.mintInfo.nuts[15].methods.length > 0) {
-			return { supported: true, params: this.mintInfo.nuts[15].methods };
+		if (this._mintInfo.nuts[15] && this._mintInfo.nuts[15].methods.length > 0) {
+			return { supported: true, params: this._mintInfo.nuts[15].methods };
 		}
 		return { supported: false };
 	}
 
 	get contact() {
-		return this.mintInfo.contact;
+		return this._mintInfo.contact;
 	}
 
 	get description() {
-		return this.mintInfo.description;
+		return this._mintInfo.description;
 	}
 
 	get description_long() {
-		return this.mintInfo.description_long;
+		return this._mintInfo.description_long;
 	}
 
 	get name() {
-		return this.mintInfo.name;
+		return this._mintInfo.name;
 	}
 
 	get pubkey() {
-		return this.mintInfo.pubkey;
+		return this._mintInfo.pubkey;
 	}
 
 	get version() {
-		return this.mintInfo.version;
+		return this._mintInfo.version;
 	}
 
 	get motd() {
-		return this.mintInfo.motd;
+		return this._mintInfo.motd;
 	}
 }
