42998e5c6527575de607cc03c0a58e63f60883a9
Egge <egge@getcurrent.io>
2024-11-27 10:13:57 +0000

    moved to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,41 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import pkg from './package.json';
+import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-	build: {
-		outDir: 'lib',
-		target: 'esnext',
-		lib: {
-			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'cashuts',
-			// the proper extensions will be added
-			fileName: 'cashuts',
-			formats: ['es', 'cjs', 'iife']
-		},
-		rollupOptions: {
-			// make sure to externalize deps that shouldn't be bundled
-			// into your library
-			output: {},
-			external: ['@cashu/crypto']
-		}
-	}
-});
+const config =
+	process.env.BUILD_FORMAT === 'iife'
+		? defineConfig({
+				build: {
+					outDir: 'lib',
+					target: 'es2020',
+					lib: {
+						entry: resolve(__dirname, 'src/index.ts'),
+						name: 'cashuts',
+						formats: ['iife'],
+						fileName: (format) => `cashu-ts.${format}.js`
+					}
+				},
+				plugins: [dts({ tsconfigPath: './tsconfig.json', outDir: 'lib/types' })]
+		  })
+		: defineConfig({
+				build: {
+					outDir: 'lib',
+					target: 'es2020',
+					lib: {
+						entry: resolve(__dirname, 'src/index.ts'),
+						name: 'cashuts',
+						fileName: (format) => `cashu-ts.${format}.js`,
+						formats: ['es', 'cjs']
+					},
+					rollupOptions: {
+						output: {},
+						external: (id) => {
+							return Object.keys((pkg as any).dependencies).includes(id);
+						}
+					}
+				},
+				plugins: [dts({ tsconfigPath: './tsconfig.json', outDir: 'lib/types' })]
+		  });
+
+export default config;
