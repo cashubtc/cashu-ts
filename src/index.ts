847ed87d3bbce77afd285b964e0b9f94630cff47
Egge <egge@getcurrent.io>
2024-12-20 06:45:52 +0100

    added OutputData to public API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { CashuMint } from './CashuMint.js';
 import { CashuWallet } from './CashuWallet.js';
 import { PaymentRequest } from './model/PaymentRequest.js';
+import { OutputData } from './model/OutputData.js';
 import { setGlobalRequestOptions } from './request.js';
 import {
 	getEncodedToken,
@@ -16,6 +17,7 @@ export {
 	CashuMint,
 	CashuWallet,
 	PaymentRequest,
+	OutputData,
 	getDecodedToken,
 	getEncodedToken,
 	getEncodedTokenV4,
