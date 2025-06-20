79b6cabc4ec099883e0d8d0ab059fc9baa70a3ed
lollerfirst <lollerfirst@gmail.com>
2025-06-20 12:35:47 +0200

    fixes

--- src/CashuWallet.ts
+++ src/CashuWallet.ts
@@ -491,7 +491,7 @@ class CashuWallet {
 
 		// include the fees to spend the the outputs of the swap
 		// input selection, needs fees because of the swap
-		const { send: sendProofs } = this.selectProofsToSend(
+		const { keep: keepProofs, send: sendProofs } = this.selectProofsToSend(
 			proofsToSend,
 			sendAmounts.reduce((acc, v) => acc + v, 0),
 			true
@@ -567,7 +567,7 @@ class CashuWallet {
 			}
 		});
 		return {
-			keep: splitProofsToKeep,
+			keep: [...splitProofsToKeep, ...keepProofs],
 			send: splitProofsToSend
 		};
 	}
