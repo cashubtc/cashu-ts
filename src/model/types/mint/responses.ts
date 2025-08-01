cf6d01d46c37c1a36b8a0d18eecb4a9180fbf76a
d4rp4t <d4rp4t@tutanota.com>
2025-08-01 11:39:05 +0200

    better types & maping null -> infinity, ttl unit conversion

--- src/model/types/mint/responses.ts
+++ src/model/types/mint/responses.ts
@@ -105,7 +105,8 @@ export type GetInfoResponse = {
 			// WebSockets
 			supported: Array<WebSocketSupport>;
 		};
-		'19'?: Nut19Policy;
+		// Cached mint responses, tll is mapped null -> infinity later
+		'19'?: Omit<Nut19Policy, "ttl"> & { ttl: number | null };
 		'20'?: {
 			// Locked Mint Quote
 			supported: boolean;
@@ -315,6 +316,6 @@ export type BlindAuthMintResponse = {
  *  Caching policy of mint endpoints
  */
 export type Nut19Policy = {
-	ttl: number | null;
+	ttl: number;
 	cached_endpoints: Array<{ method: 'GET' | 'POST'; path: string }>;
-} | null;
+};
