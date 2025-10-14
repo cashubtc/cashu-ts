b71b04b1e707ea66a857746d6222991cdbc298e5
asmo <asmogo@protonmail.com>
2025-10-14 08:46:07 +0000

    update comment

--- src/CashuWallet.ts
+++ src/CashuWallet.ts
@@ -205,7 +205,7 @@ class CashuWallet {
 	getActiveKeyset(keysets: MintKeyset[]): MintKeyset {
 		let activeKeysets = keysets.filter((k: MintKeyset) => k.active && k.unit === this._unit);
 
-		// we only consider keyset IDs that start with "00"
+		// we only consider keyset IDs that start with "00" or "01"
 		activeKeysets = activeKeysets.filter((k: MintKeyset) => k.id.startsWith('00') || k.id.startsWith('01'));
 
 		const activeKeyset = activeKeysets.sort(
