530234d54c4581a8259dc5d344a6ddf40c5e9da2
lemonknowsall <lemonknowsall@gmail.com>
2025-12-18 22:47:16 -0800

    use constant-time field operations in createDLEQProof

--- src/crypto/mint/NUT12.ts
+++ src/crypto/mint/NUT12.ts
@@ -1,8 +1,7 @@
 import { hash_e, createRandomPrivateKey, type DLEQ } from '../common/index';
 import { type WeierstrassPoint } from '@noble/curves/abstract/weierstrass.js';
-import { bytesToHex, numberToBytesBE } from '@noble/curves/utils.js';
+import { numberToBytesBE } from '@noble/curves/utils.js';
 import { secp256k1 } from '@noble/curves/secp256k1.js';
-import { bytesToNumber, hexToNumber } from '../util/utils';
 
 /**
  * !!! WARNING !!! Not recommended for production use, due to non-constant time operations See:
@@ -10,16 +9,16 @@ import { bytesToNumber, hexToNumber } from '../util/utils';
  * https://en.wikipedia.org/wiki/Timing_attack for information about timing attacks.
  */
 export const createDLEQProof = (B_: WeierstrassPoint<bigint>, a: Uint8Array): DLEQ => {
-	const r = bytesToHex(createRandomPrivateKey()); // r <- random
-	const n_r = hexToNumber(r);
-	const R_1 = secp256k1.Point.BASE.multiply(n_r); // R1 = rG
-	const R_2 = B_.multiply(n_r); // R2 = rB_
-	const C_ = B_.multiply(bytesToNumber(a)); // C_ = aB_
-	const n_a = bytesToNumber(a);
-	const A = secp256k1.Point.BASE.multiply(n_a); // A = aG
+	const r = secp256k1.Point.Fn.fromBytes(createRandomPrivateKey()); // r <- random (Uint8Array)
+	const R_1 = secp256k1.Point.BASE.multiply(r); // R1 = rG
+	const R_2 = B_.multiply(r); // R2 = rB_
+	const scalar_a = secp256k1.Point.Fn.fromBytes(a);
+	const C_ = B_.multiply(scalar_a); // C_ = aB_
+	const A = secp256k1.Point.BASE.multiply(scalar_a); // A = aG
 	const e = hash_e([R_1, R_2, A, C_]); // e = hash(R1, R2, A, C_)
-	const n_e = bytesToNumber(e);
-	// WARNING: NON-CONSTANT TIME OPERATIONS?
-	const s = numberToBytesBE((n_r + n_e * n_a) % secp256k1.Point.CURVE().n, 32); // (r + ea) mod n
+	const scalar_e = secp256k1.Point.Fn.fromBytes(e);
+	// Use field operations for constant-time addition and multiplication
+	const s_scalar = secp256k1.Point.Fn.add(r, secp256k1.Point.Fn.mul(scalar_e, scalar_a));
+	const s = numberToBytesBE(s_scalar, 32); // s = (r + e * a) mod n
 	return { s, e };
 };
