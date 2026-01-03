4e3f79afd78d05a8dd4f5326d6611b8456db7aa1
callebtc <93376500+callebtc@users.noreply.github.com>
2026-01-03 16:03:58 +0700

    format

--- src/utils/core.ts
+++ src/utils/core.ts
@@ -175,8 +175,8 @@ export function hasCorrespondingKey(amount: number, keyset: Keys): boolean {
 /**
  * Converts a bytes array to a number.
  *
- * @deprecated Use {@link Bytes.toBigInt} instead for better performance.
- *             This function is kept for backward compatibility.
+ * @deprecated Use {@link Bytes.toBigInt} instead for better performance. This function is kept for
+ *   backward compatibility.
  * @param bytes To convert to number.
  * @returns Number as bigint.
  */
