fbe1e340946b46dae76ea2fcfa7adc18dd6e281f
TheRealCheebs <226949792+TheRealCheebs@users.noreply.github.com>
2026-02-16 16:24:13 -0500

    chore(scripts): refine branch naming for multiple mint updates

--- scripts/UpdateMakefileMintVersion.ts
+++ scripts/UpdateMakefileMintVersion.ts
@@ -51,11 +51,10 @@ function writePrOutputs(updates: Update[]): void {
 			: 'chore(docker): update mint images';
 	setGithubOutput('title', title);
 
-	const date = new Date().toISOString().replace(/\D/g, '');
 	const branch =
 		updates.length === 1
 			? `update-${updates[0].name.toLowerCase()}-${updates[0].version.replace(/[:\/]/g, '-')}`
-			: `update-mint-images-${date}`;
+			: `update-mint-images`;
 	setGithubOutput('branch', branch);
 }
 
