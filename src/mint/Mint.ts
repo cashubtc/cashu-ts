c6e35ed445c88d3afe4ef8d1df8020d066b8dbde
Rob Woodgate <robwoodgate@mac.com>
2026-02-25 19:51:24 +0000

    fix(mint): normalize melt and restore amounts
    Normalize melt and restore amounts to prevent bigint leaks.

--- src/mint/Mint.ts
+++ src/mint/Mint.ts
@@ -65,6 +65,7 @@ import {
 	type MintQuoteBolt11Request,
 	type MintQuoteBolt12Request,
 	type SwapRequest,
+	type SerializedBlindedMessage,
 	type SerializedBlindedSignature,
 } from '../model/types';
 import { Amount, type AmountLike } from '../model/Amount';
@@ -486,7 +487,7 @@ class Mint {
 			throw new Error('Invalid response from mint');
 		}
 
-		return data;
+		return this.normalizeMeltBaseResponse(data);
 	}
 
 	/**
@@ -647,6 +648,8 @@ class Mint {
 			throw new Error('Invalid response from mint');
 		}
 
+		data.outputs = this.normalizeMessageAmounts(data.outputs, 'restore.outputs');
+		data.signatures = this.normalizeSignatureAmounts(data.signatures, 'restore.signatures');
 		return data;
 	}
 
@@ -819,6 +822,16 @@ class Mint {
 				amount: this.normalizeAmountOrThrow(signature.amount, `${context}.amount`),
 			};
 		});
+	}
+
+	private normalizeMessageAmounts(
+		messages: SerializedBlindedMessage[],
+		context: string,
+	): SerializedBlindedMessage[] {
+		return messages.map((message) => ({
+			...message,
+			amount: this.normalizeAmountOrThrow(message.amount, `${context}.amount`),
+		}));
 	}
 
 	private normalizeMintQuoteBolt11Response(
