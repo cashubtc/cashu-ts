6db0716e55cc7bc73730387a1ba33091b180144e
Rob Woodgate <robwoodgate@mac.com>
2026-03-05 23:51:24 +0000

    fix(mintinfo): validate NUT-19 cached_endpoints before using it

--- src/model/MintInfo.ts
+++ src/model/MintInfo.ts
@@ -123,7 +123,7 @@ export class MintInfo {
 
 	private checkNut19() {
 		const rawPolicy = this._mintInfo.nuts?.[19];
-		if (rawPolicy && rawPolicy.cached_endpoints.length > 0) {
+		if (rawPolicy && (rawPolicy?.cached_endpoints?.length || 0) > 0) {
 			return {
 				supported: true,
 				params: {
