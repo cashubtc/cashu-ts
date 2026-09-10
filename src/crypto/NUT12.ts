24ea3f761a323ef1b4c5118567c6ae8d47d9a84f
Rob Woodgate <robwoodgate@mac.com>
2026-09-10 01:49:45 +0100

    fix(crypto): keep createDLEQProof's public parameter name

--- src/crypto/NUT12.ts
+++ src/crypto/NUT12.ts
@@ -68,8 +68,8 @@ export const verifyDLEQProof_reblind = (
  * https://github.com/cashubtc/cashu-crypto-ts/pull/2 for more details See:
  * https://en.wikipedia.org/wiki/Timing_attack for information about timing attacks.
  */
-export const createDLEQProof = (blinded: WeierstrassPoint<bigint>, a: Uint8Array): DLEQ => {
-  const B_ = assertSecpPoint(blinded); // the blinded message, as a checked point
+export const createDLEQProof = (B_: WeierstrassPoint<bigint>, a: Uint8Array): DLEQ => {
+  B_ = assertSecpPoint(B_); // the point type is structural, so parse before multiplying
   const scalar_a = secp256k1.Point.Fn.fromBytes(a);
   const A = secp256k1.Point.BASE.multiply(scalar_a); // A  = aG
   const C_ = B_.multiply(scalar_a); // C_ = aB_
