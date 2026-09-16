c6d9bcb330b6fc6287702fb445614e1bec402d1d
Rob Woodgate <robwoodgate@mac.com>
2026-09-16 19:19:05 +0100

    refactor: route the last UTF-8 decodes through utils/bytes [backport v4-dev]
    v4 has two sites: the JWT payload and the binary token magic.

--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -1,5 +1,6 @@
 import { CTSError } from '../model/Errors';
 
+// eslint-disable-next-line no-restricted-syntax -- this module owns the decoders
 const fieldUtf8Decoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
 /**
@@ -18,6 +19,7 @@ export function decodeUtf8Field(bytes: Uint8Array): string {
   }
 }
 
+// eslint-disable-next-line no-restricted-syntax -- this module owns the decoders
 const documentUtf8Decoder = new TextDecoder('utf-8', { fatal: true });
 
 /**
